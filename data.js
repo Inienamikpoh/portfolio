6f51917a2ff5e03df87fd7a806d203f2de936958
InieNamikpoh <officialjoelnamix@gmail.com>
2023-09-12 12:21:07 +0100

    yes

--- data.js
+++ data.js
@@ -7,11 +7,11 @@ const servicesData = [
     {
         title: "Responsive Layout",
         category: "responsive",
-        description: ['Here is where i excel at and have taking a liking or should i say fonding to it. Rarely many things make me happier than seeing a website with a good layout and awesome interactions.','A website isnt complete ', 'I like to visualize anything woking well']
+        description: ['Responsiveness should be a handy tool for all web developers and i strive to employ that as well','A website isnt complete if it cant meet the need of every device and show case its ability', 'I like to visualize anything woking well']
     },
     {
         title: "Fullstack Development",
         category: "fullstack",
-        description: ['Here is where i excel at and have taking a liking or should i say fonding to it. Rarely many things make me happier than seeing a website with a good layout and awesome interactions.','Its quite fulfilling and is very rewarding', 'I']
+        description: ['Just as impressiv e is a beautiful layout is a system that carries out most of its acivities more auntomatic than manual so the knowledge and use of the backend to complement the frontend in a full stack project, appeals to me.','Its quite fulfilling and is very rewarding', 'It is my top specialization']
     },
 ]
