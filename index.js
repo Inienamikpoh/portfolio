28996b55f0509c868184b119c8f04a8b6b0290bf
InieNamikpoh <officialjoelnamix@gmail.com>
2023-09-12 13:19:29 +0100

    yes

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const servicesButtons = document.querySelectorAll('.service__item');
-const serviceDetails = document.querySelector('.services__right')
+const serviceDetails = document.querySelector('.services__right');
 
 
 const getService = (category) => {
     const details = servicesData.find(item => item.category === category);
     serviceDetails.innerHTML = `
     <h3>${details.title}</h3>
-    ${details.description.map(paragraph => "<p>" + paragraph + "</p>")}
+    ${details.description.map(paragraph => "<p>" + paragraph + "</p>") .join('')}
     `
 }
+
+ const removeActiveClass = () => {
+    servicesButtons.forEach(button => {
+        button.classList.remove('active');
+    })
+ }
 
 servicesButtons.forEach(item => {
     item.addEventListener('click', () => {
         const serviceClass = item.classList[1];
         getService(serviceClass)
+        item.classList.add('active')
     })
 })
