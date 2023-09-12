0b0b465899d12cf973f71a841ad5d6c8c7436e6c
InieNamikpoh <officialjoelnamix@gmail.com>
2023-09-12 11:33:55 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['Here is where i excel at and have taking a liking or should i say fonding to it. Rarely many things make me happier than seeing a website with a good layout and awesome interactions.','Its quite fulfilling and is very rewarding', 'It is my top specialization']
+    },
+    {
+        title: "Responsive Layout",
+        category: "responsive",
+        description: ['Here is where i excel at and have taking a liking or should i say fonding to it. Rarely many things make me happier than seeing a website with a good layout and awesome interactions.','A website isnt complete ', 'I like to visualize anything woking well']
+    },
+    {
+        title: "Fullstack Development",
+        category: "fullstack",
+        description: ['Here is where i excel at and have taking a liking or should i say fonding to it. Rarely many things make me happier than seeing a website with a good layout and awesome interactions.','Its quite fulfilling and is very rewarding', 'I']
+    },
+]
